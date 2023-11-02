a9ce5d43d0472bf03e52df10698f004c3b944f79
Marta Dychała <mar754dyc@gmail.com>
2023-11-03 00:53:14 +0100

    Update react query, add eslint rules for the react query (#29)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { library, config } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { Toaster } from 'react-hot-toast';
@@ -13,36 +15,40 @@ import '~/styles/Calendar.css';
 config.autoAddCss = false;
 library.add(fas);
 
+const queryClient = new QueryClient();
+
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps }
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-      <Toaster
-        toastOptions={
-          {
-            // success: {
-            //   style: {
-            //     background: 'green',
-            //     color: 'white'
-            //   }
-            // },
-            // error: {
-            //   style: {
-            //     background: 'red',
-            //     color: 'white'
-            //   },
-            //   iconTheme: {
-            //     primary: 'white',
-            //     secondary: 'red'
-            //   }
-            // }
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+        <Toaster
+          toastOptions={
+            {
+              // success: {
+              //   style: {
+              //     background: 'green',
+              //     color: 'white'
+              //   }
+              // },
+              // error: {
+              //   style: {
+              //     background: 'red',
+              //     color: 'white'
+              //   },
+              //   iconTheme: {
+              //     primary: 'white',
+              //     secondary: 'red'
+              //   }
+              // }
+            }
           }
-        }
-      />
-    </SessionProvider>
+        />
+      </SessionProvider>
+    </QueryClientProvider>
   );
 };
 
